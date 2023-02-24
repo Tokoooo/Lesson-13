a2e5f656763c298b8fc7cddb316decafe511122d
Tornike Shatberashvili <121308808+Tokoooo@users.noreply.github.com>
2023-02-24 13:34:24 +0400

    Javscript

---
+++ app.js
@@ -0,0 +1,8 @@
+let myName = 'Tornike'
+let myHobby = ['coding', 'skiing', 'drinking :D']
+let yearOfBirth = 2003
+let currentYear = 2023
+
+let combined = `My name is ${myName}, I am ${currentYear - yearOfBirth} years old and my hobby is ${myHobby}`
+
+console.log(combined)
